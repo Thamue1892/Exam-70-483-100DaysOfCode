193b8bc35ad4880d6d4115b17073a382912ef451
Thaymes <thamue1892@gmail.com>
2018-01-05 02:51:19 +0200

    ThreadPool_Begin

---
+++ ThreadLocal_T/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadLocal_T
+{
+    class Program
+    {
+        public static ThreadLocal<int> _field =
+            new ThreadLocal<int>(() =>
+            {
+                return Thread.CurrentThread.ManagedThreadId;
+            });
+
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int x = 0; x < _field.Value; x++)
+                {
+                    Console.WriteLine("ThreadA:{0}", x);
+                }
+            }).Start();
+            new Thread(() =>
+                {
+                    for (int x = 0; x < _field.Value; x++)
+                    {
+                        Console.WriteLine("ThreadB:{0}", x);
+                    }
+                }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
+
--- ThreadStaticAttribute/Program.cs
+++ ThreadStaticAttribute/Program.cs
@@ -8,8 +8,29 @@ namespace ThreadStaticAttribute
 {
     class Program
     {
-        static void Main(string[] args)
+        [ThreadStatic]
+        public static int _field;
+        public static void Main()
         {
+            new Thread(() =>
+            {
+            for (int x = 0; x < 10; x++)
+            {
+                _field++;
+                Console.WriteLine(“ThreadA:{ 0}”, _field);
         }
+    }).Start();
+    new Thread(()=>
+         for(int x= 0; x<10; x++)
+                    {
+                        _field++;
+                        Console.WriteLine(“Thread B: {0}”, _field);
+                    }
+                }).Start();
+Console.ReadKey();
+  
+        static void Main(string[] args)
+{
+}
     }
 }
