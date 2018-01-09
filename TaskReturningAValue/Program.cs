deb24fbfe71594741d633d5d1972da5a3fb0bf69
ThaymesCoder <tmpofu@thami.co.za>
2018-01-09 04:41:35 +0200

    Task<T>,returning a value,begin

---
+++ TaskReturningAValue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskReturningAValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+              {
+                  return 42;
+              });
+
+            Console.WriteLine(t.Result);
+        }
+    }
+}
