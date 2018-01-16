fd14cb34dc2b66270da72482b96160c6747f2c86
ThaymesCoder <tmpofu@thami.co.za>
2018-01-16 15:40:03 +0200

    Using the 0background worker class

---
+++ UsingBackgroundWorkerClass/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace UsingBackgroundWorkerClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.Location = new System.Drawing.Point(36, 42);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(31, 15);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "label";
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(36, 90);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "button";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ UsingBackgroundWorkerClass/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace UsingBackgroundWorkerClass
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker worker;
+        public Form1()
+        {
+            InitializeComponent();
+
+            worker = new BackgroundWorker();
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(
+                new Action<string>(UpdateLabel),
+                e.Result.ToString());
+            }
+            else
+            {
+                UpdateLabel(e.Result.ToString());
+            }
+        }
+
+        private void UpdateLabel(string text)
+        {
+            lblResult.Text = text;
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            e.Result = DoIntensiveCalculations();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!worker.IsBusy)
+            {
+                worker.RunWorkerAsync();
+            }
+        }
+
+        private object DoIntensiveCalculations()
+        {
+            //We are simulating intensive calculations
+            //by doing nonsense divisions.
+            double result = 1000000000d;
+            var maxValue = Int32.MaxValue;
+            for (int i = 1; i < maxValue; i++)
+            {
+                result /= i;
+            }
+            return result + 10d;
+        }
+    }
+}
